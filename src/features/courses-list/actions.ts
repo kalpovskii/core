50b47ad8637fdbfa5adc432a702b666aa394ca03
kalpovski <kalpovskii@gmail.com>
2024-10-31 17:51:06 +0300

    :recycle: import types into components

--- src/features/courses-list/actions.ts
+++ src/features/courses-list/actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { courseRepository } from "./ui/courses.repository";
+import { CreateCourseListElementCommand } from "./model/types";
 
 export const createCourseAction = async (
   command: CreateCourseListElementCommand,
