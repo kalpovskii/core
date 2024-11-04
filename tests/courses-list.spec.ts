888b8017046ef43e037d19bffee039c4c85e4966
kalpovski <kalpovskii@gmail.com>
2024-11-04 10:15:53 +0300

    :heavy_plus_sign: Add playwright and test CI

---
+++ tests/courses-list.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("create delete course list", async ({ page }) => {
+  await page.goto("/");
+  await page.getByPlaceholder("название").click();
+  await page.getByPlaceholder("название").fill("test course");
+  await page.getByPlaceholder("описание").click();
+  await page.getByPlaceholder("описание").fill("test description");
+  await page.getByRole("button", { name: "Добавить" }).click();
+  await expect(
+    page.getByText("test coursetest descriptionУдалить")
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "Удалить" }).click();
+
+  await expect(
+    page.getByText("test coursetest descriptionУдалить")
+  ).not.toBeVisible();
+});
